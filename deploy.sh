10aca199e6882eb784f1e1cb0677ebc1f26837a3
Eduard Knyshov <edvorg@gmail.com>
2018-12-15 21:25:16 +0800

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -13,10 +13,11 @@ rm -rf target/deploy
 cargo clean
 cargo clippy -- -D warnings
 cargo clean
-cargo web build --bin correlation_worker --target wasm32-unknown-unknown --release
-cp -f target/wasm32-unknown-unknown/release/correlation_worker.js ./static/
-cp -f target/wasm32-unknown-unknown/release/correlation_worker.wasm ./static/
-cargo web deploy --release
+cargo web build -p rustmith_correlation_worker --target wasm32-unknown-unknown --release
+cp -f target/wasm32-unknown-unknown/release/rustmith_correlation_worker.js ./frontend/static/
+cp -f target/wasm32-unknown-unknown/release/rustmith_correlation_worker.wasm ./frontend/static/
+cargo clean
+cargo web deploy -p rustmith_frontend --target wasm32-unknown-unknown --release
 
 git checkout gh-pages
 mv -f target/deploy/* ./
@@ -25,9 +26,9 @@ RUSTMITH_CSS_CHECKSUM=$(md5sum index.css | awk '{print $1}' | xargs echo -n)
 RUSTMITH_JS_CHECKSUM=$(md5sum rustmith.js | awk '{print $1}' | xargs echo -n)
 RUSTMITH_WASM_CHECKSUM=$(md5sum rustmith.wasm | awk '{print $1}' | xargs echo -n)
 
-sed -i "s/rustmith.js/rustmith.js?hash=${RUSTMITH_JS_CHECKSUM}/g" index.html
+sed -i "s/rustmith_frontend.js/rustmith_frontend.js?hash=${RUSTMITH_JS_CHECKSUM}/g" index.html
 sed -i "s/index.css/index.css?hash=${RUSTMITH_CSS_CHECKSUM}/g" index.html
-sed -i "s/rustmith.wasm/rustmith.wasm?hash=${RUSTMITH_WASM_CHECKSUM}/g" rustmith.js
+sed -i "s/rustmith_frontend.wasm/rustmith_frontend.wasm?hash=${RUSTMITH_WASM_CHECKSUM}/g" rustmith_frontend.js
 
 git add *.css
 git add *.html
